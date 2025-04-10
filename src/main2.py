0697003b6180f880aa43b6fc2473e1327ab01831
rafaelMachado1891 <rafadmn1@gmail.com>
2025-04-10 17:20:11 -0300

    uodate

--- src/main2.py
+++ src/main2.py
@@ -1,2 +1,13 @@
-import certifi
-print(certifi.where())
+import pandas as pd
+
+df= pd.read_csv('../dados/EXP_2024.csv',sep=";")
+
+NCM = ['49029000''82119400''85392910''94051190''94052900''94054900''94059200']
+
+linhas_filtradas =  df['CO_NCM'].isin(NCM)
+
+print(linhas_filtradas)
+
+df = df[linhas_filtradas]
+
+print(df)
